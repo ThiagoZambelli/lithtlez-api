275df40e4860c06a05c744a1f55e85c8e098a53a
ThiagoZambelli <thiagozamoliveira@gmail.com>
2023-07-27 17:19:25 -0300

    fix: correção de redundancia de email

--- src/models/User.js
+++ src/models/User.js
@@ -5,12 +5,13 @@ const userSchema = new mongoose.Schema(
     id: {type:String},
     nome: {type:String, required: [true, "O nome é obrigatório"]},
     endereco: {type:String, required: [true, "O endereço é obrigatório"]},
-    email: {type:String, unique:true, required: [true, "O email é obrigatório"]},
+    email: {type:String,  required: [true, "O email é obrigatório"]},
     senha: {type:String, required: [true, "A senha é obrigatório"]},
     cep: {type:String, required: [true, "O CEP é obrigatório"]},
     complemento: {type:String, required: [true, "O complemento é obrigatório"]}    ,
     itensFavoritos: {type: mongoose.Schema.Types.ObjectId, ref:"itens"}    
   }
+  // unique:true,
 );
 
 const users = mongoose.model("users", userSchema);
