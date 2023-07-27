b3c7bc76454f4c9cab94fa26b4b084071768b391
ThiagoZambelli <thiagozamoliveira@gmail.com>
2023-07-27 18:02:40 -0300

    fix: teste

--- src/controllers/UserController.js
+++ src/controllers/UserController.js
@@ -24,7 +24,7 @@ class UserController {
 
     try {
       if (emailJaExiste.length > 0) {        
-        res.status(422).json({ menssage: "Este Email ja foi cadastrado!" });
+        res.status(206).json({ menssage: "Este Email ja foi cadastrado!" });
       } else {
         await user.save();
         res.status(201).json({ menssage: "Usuario cadastrado com sucesso." });
