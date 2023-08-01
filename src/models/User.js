1e038ea7dd89db2bdffb7daad3cc8722d5797680
ThiagoZambelli <thiagozamoliveira@gmail.com>
2023-08-01 08:55:41 -0300

    fiz: refatoraçao do moodelo user

--- src/models/User.js
+++ src/models/User.js
@@ -2,14 +2,15 @@ import mongoose from "mongoose";
 
 const userSchema = new mongoose.Schema(
   {
-    id: {type:String},
-    nome: {type:String, required: [true, "O nome é obrigatório"]},
-    endereco: {type:String, required: [true, "O endereço é obrigatório"]},
-    email: {type:String,  required: [true, "O email é obrigatório"]},
-    senha: {type:String, required: [true, "A senha é obrigatório"]},
-    cep: {type:String, required: [true, "O CEP é obrigatório"]},
-    complemento: {type:String, required: [true, "O complemento é obrigatório"]}    ,
-    itensFavoritos: {type: mongoose.Schema.Types.ObjectId, ref:"itens"}    
+    id: { type: String },
+    nome: { type: String, required: [true, "O nome é obrigatório"] },
+    email: { type: String, required: [true, "O email é obrigatório"] },
+    //                      faz com que o campo n seja apresentado em uma pesquisa
+    senha: { type: String, select: false, required: [true, "A senha é obrigatório"] },
+    itensFavoritos: { type: [mongoose.Schema.Types.ObjectId], ref: "itens" },
+    endereco: { type: String, required: [false, "O endereço é obrigatório"] },
+    cep: { type: String, required: [false, "O CEP é obrigatório"] },
+    complemento: { type: String, required: [false, "O complemento é obrigatório"] }
   }
   // unique:true,
 );
