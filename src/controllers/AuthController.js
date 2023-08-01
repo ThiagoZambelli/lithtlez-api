514264fbc004f0d56d91708264217da3376b032d
ThiagoZambelli <thiagozamoliveira@gmail.com>
2023-08-01 15:24:45 -0300

    Feat: criação de componente de login

--- src/controllers/AuthController.js
+++ src/controllers/AuthController.js
@@ -1,15 +1,23 @@
-// import bcrypt from "bcrypt";
+import bcrypt from "bcrypt";
 import User from "../models/User.js";
 
 class AuthController {
   static login = async (req, res, next) => {
-    const { email} = req.body;
-
+    const { email, senha } = req.body;
+    if (!email || !senha) {
+      res.status(300).send({ message: "Os campos de email e senha são obrigatórios" });
+    }
     try {
       //                                                *Ira traser a senha que o model bloqueou o get()
       const user = await User.findOne({ email: email }).select("+senha");
-      // const senhaIsValid = bcrypt.compare(senha, user.senha);
-      res.status(200).json(user);
+      if (!user) {
+        res.status(202).send({ message: "Email ou senha Invalidos" });
+      }
+      const senhaIsValid = await bcrypt.compare(senha, user.senha);
+      if (!senhaIsValid) {
+        res.status(202).send({ message: "Email ou senha Invalidos" });
+      }
+      res.status(200).send({ message: "Login efetuado!" });
     }
     catch (err) {
       next(err);
